7cd648f001318580f86dc57c6079280e55fecca8
Daniel <damm172004@gmail.com>
2024-05-11 11:06:46 -0400

    Modelo creado

--- blogs/models.py
+++ blogs/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Post(models.Model): 
+    title = models.CharField(max_length = 120)
+    body = models.TextField()
+    author = models.ForeignKey(
+        "auth.User", 
+        on_delete = models.CASCADE #Comportamiento para borrar todo el contenido de ese autor
+    )
+
+    def __str__(self) -> str: #Este metodo sirve para mostrar el titulo de los post de la tabla
+        return self.title
